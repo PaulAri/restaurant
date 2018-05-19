8b2926aab6fea75ef6ab65e7257af74e1ef6a007
PaulAri <paulari97@yahoo.com>
2018-05-19 11:17:53 +0300

    commmmmit

--- newapp/models.py
+++ newapp/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Produs (models.Model):
     title = models.CharField(max_length=255)
-    pret = models.IntegerField
-    cantitate = models.IntegerField
+    pret = models.IntegerField()
+    cantitate = models.IntegerField()
 
     owner = models.ForeignKey("auth.User", models.CASCADE, null=True, blank=True)
 
